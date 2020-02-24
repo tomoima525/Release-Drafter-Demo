3bd0a92fe7721d078068d0b85230663edef7f1a8
Esed <esed.alih@gmail.com>
2020-02-24 22:04:23 +0100

    Add test for exit button visibility

--- app/src/androidTest/java/ba/app/sampleandroidapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ba/app/sampleandroidapp/ExampleInstrumentedTest.kt
@@ -1,12 +1,16 @@
 package ba.app.sampleandroidapp
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +19,21 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("ba.app.sampleandroidapp", appContext.packageName)
+    }
+
+    @Rule
+    var mActivityRule: ActivityTestRule<MainActivity> = ActivityTestRule(
+        MainActivity::class.java
+    )
+
+    @Test
+    fun textExitButtonVisibility() {
+        onView(withId(R.id.exit_button)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
     }
 }
